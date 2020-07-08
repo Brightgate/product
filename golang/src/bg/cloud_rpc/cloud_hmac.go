b69ea0668044085878d0793e92d59304dd93434d
Stephen Hahn <stephen@brightgate.com>
2020-07-08 17:44:52 +0000

    copyright cleanup
    Summary:
    This changeset fixes the missing or incorrectly formatted copyright blocks in
    Brightgate-authored files.
    
    Test Plan: Built.
    
    Reviewers: duvall
    
    Reviewed By: duvall
    
    Differential Revision: https://ph0.b10e.net/D1072

--- golang/src/bg/cloud_rpc/cloud_hmac.go
+++ golang/src/bg/cloud_rpc/cloud_hmac.go
@@ -1,3 +1,13 @@
+//
+// COPYRIGHT 2017 Brightgate Inc. All rights reserved.
+//
+// This copyright notice is Copyright Management Information under 17 USC 1202
+// and is included to protect this work and deter copyright infringement.
+// Removal or alteration of this Copyright Management Information without the
+// express written permission of Brightgate Inc is prohibited, and any
+// such unauthorized removal or  alteration will be a violation of federal law.
+//
+
 package cloud_rpc
 
 // These eight byte keys were generated using
--- golang/src/bg/naming/mnemonic/mnemonic.go
+++ golang/src/bg/naming/mnemonic/mnemonic.go
@@ -1,5 +1,6 @@
+//
 // COPYRIGHT 2017 Brightgate Inc.  All rights reserved.
-
+//
 // This copyright notice is Copyright Management Information under 17 USC 1202
 // and is included to protect this work and deter copyright infringement.
 // Removal or alteration of this Copyright Management Information without the
